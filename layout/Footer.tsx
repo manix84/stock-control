19d321d361646da41c4a5843618afadd418ce9a1
Rob Taylor <rob.taylor@vault.fan>
2023-10-02 23:41:56 +0100

    Adding gradient HR to footer.

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -2,16 +2,32 @@ import { styled } from "styled-components";
 
 const Footer = () => (
   <FooterComponent>
-    Rob Taylor &copy; {new Date().getFullYear()}
+    <HorizontalRule />
+    &copy; Rob Taylor {new Date().getFullYear()}
   </FooterComponent>
 );
 export default Footer;
 
+const HorizontalRule = styled.div`
+  display: flex;
+  width: 95vw;
+  height: 1px;
+  background-color: transparent;
+
+  background: linear-gradient(
+    90deg,
+    rgba(150, 150, 150, 0.1) 0%,
+    rgba(150, 150, 150, 1) 50%,
+    rgba(150, 150, 150, 0.1) 100%
+  );
+`;
+
 const FooterComponent = styled.footer`
   display: flex;
+  flex-direction: column;
   flex: 0;
   padding: 2rem 0;
-  border-top: 1px solid #eaeaea;
+  gap: 2rem;
   justify-content: center;
   align-items: center;
   a {
